c27137025bc5c3c71fe874a6d3c649a04e58e217
Franz Mejia <mejiafranz@gmail.com>
2023-06-18 00:24:41 -0400

    feat: Refactor code with different workspaces

--- variables.tf
+++ variables.tf
@@ -2,10 +2,6 @@ variable "region" {
   description = "This is the cloud hosting regions where you will be deployed."
 }
 
-variable "dev_prefix" {
-  description = "This is the environment where your webapp is deployed. qa, prod, or dev"
-}
-
-variable "prod_prefix" {
+variable "prefix" {
   description = "This is the environment where your webapp is deployed. qa, prod, or dev"
 }
